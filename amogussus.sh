b4b311d26fa343f8d6075bf78198d937c6bf354f
jasiosasio <54361578+janbenklewski@users.noreply.github.com>
2021-05-06 21:18:55 +0200

    dfg

--- amogussus.sh
+++ amogussus.sh
@@ -3,3 +3,5 @@ git clone https://github.com/janbenklewski/sussydealz.git
 sudo apt install vlc
 cd ~/sussydealz
 vlc 'The kind of deals that make you SUS.mp3'
+sudo apt remove vlc
+sudo apt purge vlc
